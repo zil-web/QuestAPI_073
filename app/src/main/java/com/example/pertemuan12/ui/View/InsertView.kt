b7db8b401059c7fcdd81ec7d891186b3d2ec3832
zil-web <126854462+zil-web@users.noreply.github.com>
2024-12-29 16:57:52 +0700

    input data mahasiswa,  nama, NIM, jenis kelamin, alamat, kelas, dan angkatan

--- app/src/main/java/com/example/pertemuan12/ui/View/InsertView.kt
+++ app/src/main/java/com/example/pertemuan12/ui/View/InsertView.kt
@@ -95,4 +95,89 @@ fun FormInput(
     Modifier = Modifier,
     onValueChange: (InsertUiEvent) -> Unit = {},
     enabled: Boolean = true
-)
+){
+    Column(
+        modifier =
+        modifier,
+        verticalArrangement = Arrangement.spacedBy(12.
+        dp)
+    ) {
+        OutlinedTextField(
+            value = insertUiEvent.nama,
+            onValueChange = { onValueChange(insertUiEvent.copy(nama = it)) },
+            label = { Text("Nama") },
+            modifier = Modifier.
+            fillMaxWidth(),
+            enabled =
+            enabled,
+            singleLine = true
+        )
+        OutlinedTextField(
+            value = insertUiEvent.nim,
+            onValueChange = { onValueChange(insertUiEvent.copy(nim = it)) },
+            label = { Text("NIM") },
+            modifier = Modifier.
+            fillMaxWidth(),
+            enabled =
+            enabled,
+            singleLine = true
+        )
+        OutlinedTextField(
+            value = insertUiEvent.jenisKelamin,
+            onValueChange = { onValueChange(insertUiEvent.copy(jenisKelamin = it)) },
+            label = { Text("Jenis Kelamin") },
+            modifier = Modifier.
+            fillMaxWidth(),
+            enabled =
+            enabled,
+            singleLine = true
+        )
+        OutlinedTextField(
+            value = insertUiEvent.alamat,
+            onValueChange = { onValueChange(insertUiEvent.copy(alamat = it)) },
+            label = { Text("Alamat") },
+            modifier = Modifier.
+            fillMaxWidth(),
+            enabled =
+            enabled,
+            singleLine = true
+        )
+        OutlinedTextField(
+            value = insertUiEvent.kelas,
+            onValueChange = { onValueChange(insertUiEvent.copy(kelas = it)) },
+            label = { Text("Kelas") },
+            modifier = Modifier.
+            fillMaxWidth(),
+            enabled =
+            enabled,
+            singleLine = true
+        )
+        OutlinedTextField(
+            value = insertUiEvent.angkatan,
+            onValueChange = { onValueChange(insertUiEvent.copy(angkatan = it)) },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+            label = { Text("Angkatan") },
+            modifier = Modifier.
+            fillMaxWidth(),
+            enabled =
+            enabled,
+            singleLine = true
+        )
+        if (
+            enabled) {
+            Text(
+                text = "Isi Semua Data!",
+                modifier = Modifier.
+                padding(12.
+                dp)
+            )
+        }
+        Divider(
+            thickness = 8.
+            dp,
+            modifier = Modifier.
+            padding(12.
+            dp)
+        )
+    }
+}
