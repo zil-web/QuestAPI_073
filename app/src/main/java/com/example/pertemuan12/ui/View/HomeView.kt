0565cc126542bf4c6e9ccee8a4a7fcfa2d409113
zil-web <126854462+zil-web@users.noreply.github.com>
2024-12-29 20:11:16 +0700

    mengatur struktur dasar layar "Home Mahasiswa" dengan menggunakan Scaffold dan CostumeTopAppBar

--- app/src/main/java/com/example/pertemuan12/ui/View/HomeView.kt
+++ app/src/main/java/com/example/pertemuan12/ui/View/HomeView.kt
@@ -15,4 +15,32 @@ fun HomeScreen(
     Modifier = Modifier,
     onDetailClick: (String) -> Unit = {},
     viewModel: HomeViewModel = viewModel(factory = PenyediaViewModel.Factory)
-)
+){
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+    Scaffold(
+        modifier =
+        modifier.
+        nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            CostumeTopAppBar(
+                title = DestinasiHome.titleRes,
+                canNavigateBack = false,
+                scrollBehavior = scrollBehavior,
+                onRefresh = {
+                    viewModel.getMhs()
+                }
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = navigateToItemEntry,
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier.
+                padding(18.
+                dp)
+            ) {
+                Icon(imageVector = Icons.Default.
+                Add, contentDescription = "Add Kontak")
+            }
+        },
+    )
