5840acd0d456d60036ae5207c227563a0da6cd03
zil-web <126854462+zil-web@users.noreply.github.com>
2024-12-29 21:19:39 +0700

    import penyediaviewmodel

--- app/src/main/java/com/example/pertemuan12/ui/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/pertemuan12/ui/viewmodel/PenyediaViewModel.kt
@@ -1,5 +1,11 @@
 package com.example.pertemuan12.ui.viewmodel
 
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.pertemuan12.MahasiswaApplications
+
 object PenyediaViewModel {
     val Factory =
         viewModelFactory {
