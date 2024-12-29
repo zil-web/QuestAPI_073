a12bd5f33c9b62c0046394452e2d6d093db6f570
zil-web <126854462+zil-web@users.noreply.github.com>
2024-12-29 16:47:35 +0700

    menampilkan elemen-elemen input formulir entri data mahasiswa, seperti TextField untuk NIM, nama, alamat,

--- app/src/main/java/com/example/pertemuan12/ui/View/InsertView.kt
+++ app/src/main/java/com/example/pertemuan12/ui/View/InsertView.kt
@@ -50,3 +50,49 @@ fun EntryMhsScreen(
         )
     }
 }
+
+@Composable
+fun EntryBody(
+    insertUiState: InsertUiState,
+    onSiswaValueChange: (InsertUiEvent) -> Unit,
+    onSaveClick: () -> Unit,
+    modifier:
+    Modifier = Modifier
+) {
+    Column(
+        verticalArrangement = Arrangement.spacedBy(18.
+        dp),
+        modifier =
+        modifier.
+        padding(12.
+        dp)
+    ) {
+        FormInput(
+            insertUiEvent =
+            insertUiState.insertUiEvent,
+            onValueChange =
+            onSiswaValueChange,
+            modifier = Modifier.
+            fillMaxWidth()
+        )
+        Button(
+            onClick =
+            onSaveClick,
+            shape = MaterialTheme.shapes.small,
+            modifier = Modifier.
+            fillMaxWidth()
+        ) {
+            Text(text = "Simpan")
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun FormInput(
+    insertUiEvent: InsertUiEvent,
+    modifier:
+    Modifier = Modifier,
+    onValueChange: (InsertUiEvent) -> Unit = {},
+    enabled: Boolean = true
+)
