b09c56571a428ad7e10031baad6f9bbec7c867e4
zil-web <126854462+zil-web@users.noreply.github.com>
2024-12-29 15:07:40 +0700

    mengambil data mahasiswa dari MahasiswaRepository dan memperbarui mhsUIState sesuai dengan hasilnya

--- app/src/main/java/com/example/pertemuan12/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/example/pertemuan12/viewmodel/HomeViewModel.kt
@@ -1,6 +1,15 @@
 package com.example.pertemuan12.viewmodel
 
+import android.net.http.HttpException
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.pertemuan12.entity.Mahasiswa
+import com.example.pertemuan12.repository.MahasiswaRepository
+import kotlinx.coroutines.launch
+import java.io.IOException
 
 sealed class HomeUiState {
     data class Success(val mahasiswa:
@@ -8,3 +17,25 @@ sealed class HomeUiState {
     object Error : HomeUiState()
     object Loading : HomeUiState()
 }
+class HomeViewModel(private val mhs:
+                    MahasiswaRepository
+) : ViewModel() {
+    var mhsUIState: HomeUiState by
+    mutableStateOf(HomeUiState.Loading)
+        private set
+    init {
+        getMhs()
+    }
+    fun getMhs() {
+        viewModelScope.
+        launch {
+            mhsUIState = HomeUiState.Loading
+            mhsUIState = try {
+                HomeUiState.Success(mhs.getMahasiswa())
+            } catch (e: IOException) {
+                HomeUiState.Error
+            } catch (e: HttpException) {
+                HomeUiState.Error
+            }
+        }
+    }
