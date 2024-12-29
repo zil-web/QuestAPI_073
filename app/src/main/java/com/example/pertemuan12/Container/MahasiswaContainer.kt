44b4966fcd8e0c6037119b49c49d2ea1443d3707
zil-web <126854462+zil-web@users.noreply.github.com>
2024-12-29 13:33:06 +0700

    dependency container yang menyediakan implementasi kontakRepository untuk mengakses data mahasiswa dari API.

--- app/src/main/java/com/example/pertemuan12/Container/MahasiswaContainer.kt
+++ app/src/main/java/com/example/pertemuan12/Container/MahasiswaContainer.kt
@@ -12,3 +12,24 @@ interface AppContainer {
     val kontakRepository:
             MahasiswaRepository
 }
+class MahasiswaContainer :
+    AppContainer {
+    private val baseUrl = "http://10.0.2.2:8000/umyTI/"
+    //localhost diganti ip kalo run di hp
+    private val json =
+        Json { ignoreUnknownKeys = true }
+    private val retrofit: Retrofit = Retrofit.Builder()
+        .addConverterFactory(json.
+        asConverterFactory("application/json".
+        toMediaType()))
+        .baseUrl(baseUrl).build()
+    private val mahasiswaService:
+            MahasiswaService by
+    lazy {
+        retrofit.create(
+            MahasiswaService::class.
+            java) }
+    override val kontakRepository:
+            MahasiswaRepository by
+    lazy {
+        NetworkKontakRepository(mahasiswaService) }
