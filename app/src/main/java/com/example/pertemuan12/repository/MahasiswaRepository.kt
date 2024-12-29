222fbaf40016170fe0cfcbc6d578e953da0d395a
zil-web <126854462+zil-web@users.noreply.github.com>
2024-12-29 14:17:12 +0700

    menyimpan string literal yang digunakan di aplikasi, untuk penerjemahan dan pemeliharaan teks.

---
+++ app/src/main/java/com/example/pertemuan12/repository/MahasiswaRepository.kt
@@ -0,0 +1,47 @@
+package com.example.pertemuan12.repository
+
+import com.example.pertemuan12.entity.Mahasiswa
+import com.example.pertemuan12.service.MahasiswaService
+import java.io.IOException
+
+interface MahasiswaRepository {
+    suspend fun getMahasiswa(): List<Mahasiswa>
+    suspend fun insertMahasiswa(mahasiswa: Mahasiswa)
+    suspend fun updateMahasiswa(nim:String, mahasiswa: Mahasiswa)
+    suspend fun deleteMahasiswa(nim: String)
+    suspend fun getMahasiswaByNim(nim: String):Mahasiswa
+}
+
+class NetworkMahasiswaRepository(
+    private val mahasiswaApiService: MahasiswaService
+) : MahasiswaRepository{
+
+    override suspend fun insertMahasiswa(mahasiswa: Mahasiswa) {
+        mahasiswaApiService.insertMahasiswa(mahasiswa)
+    }
+
+    override suspend fun updateMahasiswa(nim: String, mahasiswa: Mahasiswa) {
+        mahasiswaApiService.updateMahasiswa(nim, mahasiswa)
+    }
+
+    override suspend fun deleteMahasiswa(nim: String) {
+        try {
+            val response = mahasiswaApiService.deleteMahasiswa(nim)
+            if (!response.isSuccessful) {
+                throw IOException("Failed to delete mahasiswa. HTTP Status Code: " +
+                        "${response.code()}")
+            } else {
+                response.message()
+                println(response.message())
+            }
+        } catch (e:Exception){
+            throw e
+        }
+    }
+
+    override suspend fun getMahasiswa(): List<Mahasiswa> = mahasiswaApiService.getMahasiswa()
+
+    override suspend fun getMahasiswaByNim(nim: String): Mahasiswa {
+        return  mahasiswaApiService.getMahasiswaByNim(nim)
+        }
+}
